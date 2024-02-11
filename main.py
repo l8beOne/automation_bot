49e91d3d633a63f8377dd2fee7f0df2428d710a5
chetaleks <88185086+chetaleks@users.noreply.github.com>
2024-02-11 19:26:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import config
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Здарова')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'как сам брат'
+    )
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
