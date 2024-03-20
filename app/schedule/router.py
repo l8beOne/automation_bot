8c5f6511dde06e8f94b1fae4f8846a6ff407c130
Тимофей Хачунский <t1m.khach@yandex.ru>
2024-03-20 18:14:01 +0300

    change function "create_subject_crud" (fix problem with adding several subjects) added function "get_specific_schedule_crud" change interaction with user (using telegram_id instead of id)

--- app/schedule/router.py
+++ app/schedule/router.py
@@ -1,10 +1,11 @@
+from datetime import date
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database import get_db
-from ..schemas import ProgramSchema, ProgramBaseSchema
+from ..schemas import ProgramName, ProgramSchema, ProgramBaseSchema
 from .crud import (
     create_group_crud,
     create_program_crud,
@@ -31,9 +32,10 @@ router = APIRouter(prefix="/schedule", tags=["Schedule"])
 @router.post("/")
 async def create_schedule(
     new_schedule: ScheduleBaseSchema,
+    program_name: ProgramName,
     session: AsyncSession = Depends(get_db)
 ):
-    result = await create_schedule_crud(new_schedule, session)
+    result = await create_schedule_crud(new_schedule, program_name, session)
     if type(result) == dict and result.get("status") == "error":
         raise HTTPException(
             status_code=result.get("status_code"),
@@ -42,18 +44,23 @@ async def create_schedule(
     return result.first()
 
 
-@router.post("/subjects")
+@router.delete("/")
 async def update_subjects():
     ...
 
 
-@router.get("/{user_id}/{day}/", response_model=ScheduleSubjects,)
+@router.get("/{user_id}/{day}/", response_model=List[SubjectGetSchema])
 async def get_specific_schedule(
     user_id: int,
-    day: str,
+    day: date,
     session: AsyncSession = Depends(get_db)
 ):
     result = await get_sepcific_schedule_crud(user_id, day, session)
+    if type(result) == dict and result.get("status") == "error":
+        raise HTTPException(
+            status_code=result.get("status_code"),
+            detail=result.get("detail")
+        )
     return result.all()
 
 
--- app/users/crud.py
+++ app/users/crud.py
@@ -11,6 +11,15 @@ async def regisrtation_crud(
     new_user: UserCreateSchema,
     session: AsyncSession
 ):
+    result = await session.execute(select(User).where(User.telegram_id == new_user.telegram_id))
+    session_user = result.scalar()
+    if session_user:
+        return {
+            "status": "error",
+            "status_code": 400,
+            "detail": "User already registered, "
+                      "perhaps you want update"
+        }
     stmt = insert(User).values(**new_user.model_dump())
     await session.execute(stmt)
     await session.commit()
@@ -24,12 +33,19 @@ async def get_users_crud(session: AsyncSession):
 
 
 async def regisrtation_with_program_crud(
-    user_id: int,
+    user_telegram_id: int,
     program_name: ProgramName,
     session: AsyncSession
 ):
-    program_qwery = select(Program.id).where(Program.name == program_name)
-    program_result = await session.execute(program_qwery)
+    user_exists_qwery = await session.execute(select(User.id).where(User.telegram_id == user_telegram_id))
+    user_id = user_exists_qwery.scalar()
+    if not user_id:
+        return {
+            "status": "error",
+            "status_code": 400,
+            "detail": "No user with this telegram id"
+        }
+    program_result = await session.execute(select(Program.id).where(Program.name == program_name))
     program_id = program_result.scalar()
     if not program_id:
         return {
@@ -37,11 +53,21 @@ async def regisrtation_with_program_crud(
             "status_code": 400,
             "detail": "No program with this name"
         }
-    stmt = update(User).where(User.id == user_id).values(program_id=program_id)
-    await session.execute(stmt)
+    await session.execute(
+        update(User).where(
+            User.telegram_id == user_telegram_id
+        ).values(
+            program_id=program_id
+        )
+    )
     await session.commit()
-    user_qwery = select(User.name, Program.name).where(User.id == user_id).join(Program)
-    user_result = await session.execute(user_qwery)
+    user_result = await session.execute(
+        select(
+            User.name, Program.name
+        ).where(
+            User.telegram_id == user_telegram_id
+        ).join(Program)
+    )
     res = user_result.first()
     return {
         "status": "successfully added user to program",
@@ -53,10 +79,18 @@ async def regisrtation_with_program_crud(
 
 
 async def registration_with_group_crud(
-    user_id: int,
+    user_telegram_id: int,
     group_name: str,
     session: AsyncSession,
 ):
+    user_exists_qwery = await session.execute(select(User.id).where(User.telegram_id == user_telegram_id))
+    user_id = user_exists_qwery.scalar()
+    if not user_id:
+        return {
+            "status": "error",
+            "status_code": 400,
+            "detail": "No user with this telegram id"
+        }
     group_result = await session.execute(select(Group.id).where(Group.name == group_name))
     group_id = group_result.scalar()
     if not group_id:
@@ -91,9 +125,15 @@ async def registration_with_group_crud(
 
 
 async def get_user_crud(
-    user_id: int,
+    user_telegram_id: int,
     session: AsyncSession
 ):
-    qwery = select(User).where(User.id == user_id).join(Program)
-    result = await session.execute(qwery)
+    user = await session.execute(select(User).where(User.telegram_id == user_telegram_id).join(Program))
+    result = user.scalars().first()
+    if not result:
+        return {
+            "status": "error",
+            "status_code": 404,
+            "detail": "No user with this telegram_id"
+        }
     return result
