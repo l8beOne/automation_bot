0e305667dda7b43149a3e3d7106f15cdfba53b08
chetaleks <88185086+chetaleks@users.noreply.github.com>
2024-05-26 16:02:40 +0300

    fix: better buttons for schedule

--- bot/keyboards/reply_keyboards.py
+++ bot/keyboards/reply_keyboards.py
@@ -4,33 +4,77 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def start_buttons():
     builder = ReplyKeyboardBuilder()
-    builder.row(KeyboardButton(text="Контакты"))
-    builder.row(KeyboardButton(text="Информация Про Вышку"))
-    builder.row(KeyboardButton(text="Заказать справку"))
-    builder.row(KeyboardButton(text="Узнать расписание"))
+    builder.row(
+        KeyboardButton(text="Контакты")
+    )
+    builder.row(
+        KeyboardButton(text="Информация Про Вышку")
+    )
+    builder.row(
+        KeyboardButton(text="Узнать расписание")
+    )
     start_keyboard = builder.as_markup(resize_keyboard=True)
     return start_keyboard
 
 
 def contacts_buttons():
     builder = ReplyKeyboardBuilder()
-    builder.row(KeyboardButton(text="Учебный офис"))
+    builder.row(
+        KeyboardButton(text="Учебный офис")
+    )
     builder.row(
         KeyboardButton(text="Руководители"),
         KeyboardButton(text="Учителя")
     )
-    builder.row(KeyboardButton(text="Назад"))
+    builder.row(
+        KeyboardButton(text="Назад")
+    )
     contacts_keyboard = builder.as_markup(resize_keyboard=True)
     return contacts_keyboard
 
 
 def hse_info_buttons():
     builder = ReplyKeyboardBuilder()
-    builder.row(KeyboardButton(text="Гайд по вышке"))
+    builder.row(
+        KeyboardButton(text="Гайд по вышке")
+    )
     builder.row(
         KeyboardButton(text="Академический отпуск"),
         KeyboardButton(text="Отчисление")
     )
-    builder.row(KeyboardButton(text="Назад"))
+    builder.row(
+        KeyboardButton(text="Назад")
+    )
     contacts_keyboard = builder.as_markup(resize_keyboard=True)
     return contacts_keyboard
+
+
+def schedule_op_course_buttons():
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        KeyboardButton(text="ПАДИИ 1"),
+        KeyboardButton(text="ПМИ 1")
+    )
+    builder.row(
+        KeyboardButton(text="ПАДИИ 1"),
+        KeyboardButton(text="ПМИ 1")
+    )
+    op_course_keyboard = builder.as_markup(resize_keyboard=True)
+    return op_course_keyboard
+
+
+def schedule_day_buttons():
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        KeyboardButton(text="ПН"),
+        KeyboardButton(text="ВТ"),
+        KeyboardButton(text="СР"),
+        KeyboardButton(text="ЧТ"),
+        KeyboardButton(text="ПТ"),
+        KeyboardButton(text="СБ")
+    )
+    builder.row(
+        KeyboardButton(text="Назад")
+    )
+    day_selection_keyboard = builder.as_markup(resize_keyboard=True)
+    return day_selection_keyboard
